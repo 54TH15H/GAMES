3f7a8081184f7cdc3a4e1a0f051b939ad95f3564
sathish3855 <74714002+sathish3855@users.noreply.github.com>
2020-12-05 12:31:29 +0530

    Add files via upload

---
+++ RPS.py
@@ -0,0 +1,69 @@
+print(' ROCK PAPER SCISSOR GAME ')
+a = int(input(' ENTER 1 TO START THE GAME \n ENTER 0 TO EXIT THE GAME \n'))
+flag = 0
+if a == 1:
+    flag = True
+elif a == 0:
+    flag = False
+else:
+    print(' INVALID CHOICE ')
+
+
+while flag:
+
+    print(' ENTER ')
+    print(' r or R : ROCK \n p or P : PAPER \n s or S : SCISSOR ')
+    x = input(' PLAYER 1 : ENTER YOUR CHOICE \n')
+    y = input(' PLAYER 2 : ENTER YOUR CHOICE \n')
+    x.lower()
+    y.lower()
+
+    if x == 'r' or x == 'p' or x == 's' or y == 'r' or y == 'p' or y == 's':
+
+
+        if x == 'r':
+            if y == 's':
+                print(' PLAYER 1 WINS ')
+            elif y == 'p':
+                print(' PLAYER 2 WINS ')
+            else:
+                print(' TIE ')
+
+        elif x == 'p':
+            if y == 's':
+                print(' PLAYER 2 WINS ')
+            elif y == 'r':
+                print(' PLAYER 1 WINS ')
+            else:
+                print(' TIE ')
+
+        else:
+            if y == 'r':
+                print(' PLAYER 2 WINS ')
+            elif y == 'p':
+                print(' PLAYER 1 WINS ')
+            else:
+                print(' TIE ')
+
+        print(' ENTER 1 TO CONTINUE THE GAME \n ENTER 0 TO STOP THE GAME \n')
+        a = int(input())
+
+        if a == 1:
+            flag = True
+        elif a == 0:
+            flag = False
+        else:
+            print(' INVALID CHOICE \n')
+            print(' ENTER 0 OR 1 \n')
+            a = int(input())
+            if a == 1:
+                flag = True
+            else:
+                flag = False
+
+    else:
+        print(' YOU ENTERED WRONG CHOICE ')
+        flag = False
+
+else:
+    print(' GAME OVER ')
